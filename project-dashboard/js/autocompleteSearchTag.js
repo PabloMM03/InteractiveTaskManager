5e38021ce09755617f4af6c0c5808a373ab74db4
PabloMM03 <1V3rsus1@gmail.com>
2025-02-20 16:09:35 +0100

    Goals form new style

--- project-dashboard/js/autocompleteSearchTag.js
+++ project-dashboard/js/autocompleteSearchTag.js
@@ -84,67 +84,103 @@ function selectTag(tag, inputId, suggestionsId) {
 }
 
 //Función que actualiza la barra de progreso del form
-function updateProgress(step) {
-	const totalSteps = 6;
-	const progressBar = document.querySelector('.progress');
+function updateProgress(step, formType) {
+	const progressBars = {
+		//Objeto con selectores y total de pasos
+		task: { element: document.querySelector('.progress-task'), totalSteps: 5 },
+		goal: { element: document.querySelector('.progress-goal'), totalSteps: 4 },
+	};
 
-	const percentage = (step / totalSteps) * 100;
-	progressBar.style.width = `${percentage}%`;
-}
+	// Especificar el tipo de formulario y sus elementos
+	const progressBar = progressBars[formType]?.element;
+	const totalSteps = progressBars[formType]?.totalSteps;
 
-// Animaciones del formulario
-function nextStep(step) {
-	//Siguiente paso
-	let currentStep = document.querySelector('.step.active');
-	let nextStep = document.getElementById('step-' + step);
-
-	if (currentStep && nextStep) {
-		currentStep.style.opacity = '0'; // Inicia la animación de desaparición
-		setTimeout(() => {
-			currentStep.classList.remove('active');
-			currentStep.style.display = 'none';
-
-			nextStep.style.display = 'block';
-			setTimeout(() => {
-				nextStep.classList.add('active');
-				nextStep.style.opacity = '1'; // Muestra el nuevo paso con animación
-				updateProgress(step);
-				updateBackButton();
-			}, 50);
-		}, 300); // Espera a que termine la animación antes de cambiar
+	if (progressBar) {
+		progressBar.style.width = `${(step / totalSteps) * 100}%`;
 	}
 }
 
-function previousStep() {
-	//Paso anterior
-	let currentStep = document.querySelector('.step.active');
-	let prevStepId = parseInt(currentStep.id.split('-')[1]) - 1;
-	let prevStep = document.getElementById('step-' + prevStepId);
+// Animaciones del formulario
+function nextStep(step, formType) {
+	function typeOfStep(stepActive, stepType) {
+		//Siguiente paso
+		let currentStep = document.querySelector(stepActive);
+		let nextStep = document.getElementById(stepType + step);
 
-	if (prevStep) {
-		currentStep.style.opacity = '0';
-
-		setTimeout(() => {
-			currentStep.classList.remove('active');
-			currentStep.style.display = 'none';
-
-			prevStep.style.display = 'block';
+		if (currentStep && nextStep) {
+			currentStep.style.opacity = '0'; // Inicia la animación de desaparición
 			setTimeout(() => {
-				prevStep.classList.add('active');
-				prevStep.style.opacity = '1';
-				updateProgress(prevStepId);
-				updateBackButton();
-			}, 50);
-		}, 300);
+				currentStep.classList.remove('active');
+				currentStep.style.display = 'none';
+
+				nextStep.style.display = 'block';
+				setTimeout(() => {
+					nextStep.classList.add('active');
+					nextStep.style.opacity = '1'; // Muestra el nuevo paso con animación
+					updateProgress(step, formType);
+					updateBackButton();
+				}, 50);
+			}, 300); // Espera a que termine la animación antes de cambiar
+		}
+	}
+
+	//Comprobar de que tipo es el formulario
+
+	if (formType === 'task') {
+		typeOfStep('.step.active', 'step-');
+	} else if (formType === 'goal') {
+		typeOfStep('.step-goal.active', 'step-goal-');
+	}
+}
+
+function previousStep(formType) {
+	//Paso anterior
+
+	function typeOfPreviousStep(stepActive, stepType) {
+		let currentStep = document.querySelector(stepActive);
+		let previousStep = currentStep.previousElementSibling; // Paso anterior
+
+		if (currentStep && previousStep && previousStep.id.startsWith(stepType)) {
+			currentStep.style.opacity = '0'; // Inicia la animación de desaparición
+			setTimeout(() => {
+				currentStep.classList.remove('active');
+				currentStep.style.display = 'none';
+
+				previousStep.style.display = 'block';
+				setTimeout(() => {
+					previousStep.classList.add('active');
+					previousStep.style.opacity = '1'; // Muestra el nuevo paso con animación
+					updateProgress(previousStep.id.replace(stepType, ''), formType);
+					updateBackButton();
+				}, 50);
+			}, 300);
+		}
+	}
+
+	if (formType === 'task') {
+		typeOfPreviousStep('.step.active', 'step-');
+	} else if (formType === 'goal') {
+		typeOfPreviousStep('.step-goal.active', 'step-goal-');
 	}
 }
 
 //Mostrar el boton de volver cuando sea necesario
 function updateBackButton() {
-	const currentStep = document.querySelector('.step.active');
-	const backButton = document.querySelector('.back-button');
-	backButton.style.display =
-		parseInt(currentStep.id.split('-')[1]) > 1 ? 'block' : 'none';
+	const steps = [
+		{ selector: '.step.active', button: '.back-button-task' },
+		{ selector: '.step-goal.active', button: '.back-button-goal' },
+	];
+
+	steps.forEach(({ selector, button }) => {
+		const currentStep = document.querySelector(selector);
+		const backButton = document.querySelector(button);
+
+		if (currentStep && backButton) {
+			//Se toma el ultimo valor del selector
+			const stepNumber = parseInt(currentStep.id.split('-').pop());
+			backButton.style.display = stepNumber > 1 ? 'block' : 'none';
+		}
+	});
 }
 
 document.addEventListener('DOMContentLoaded', () => {
